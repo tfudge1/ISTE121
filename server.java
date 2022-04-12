c19e4d5fbf98549ec6bba011bf62113f60d325ee
Derek Pastor <derekjonp@gmail.com>
2022-04-12 19:48:43 -0400

    Renamed and merged

--- server.java
+++ server.java
@@ -2,15 +2,20 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 
-public class server {
-   public ArrayList<car> clientCars = new ArrayList<car>();
+public class Server{
+   public GameMaster gm;
+
 
    public static void main(String[] args) {
-      new server().execute();
+
+      new Server().execute();
    }
    
    public void execute() {
-   
+      gm=new GameMaster();
+      GameMaster.Game currentGame=gm.startANewGame();
+
+
       System.out.println("Accepting Connection on port 12345..");
       try{
          ServerSocket ss = new ServerSocket(12345);
@@ -18,7 +23,7 @@ public class server {
          while(true){
             Socket s = ss.accept();
             System.out.println("Request received from " + s.getInetAddress().getHostName());
-            new ProcessThread(s).start();
+            new ProcessThread(s, currentGame).start();
          }
       }catch(Exception ex){
          ex.printStackTrace();
@@ -29,12 +34,16 @@ public class server {
    class ProcessThread extends Thread {
       DataInputStream dis;
       DataOutputStream dos;
-      boolean ready = false;
       Socket socket;
-      
 
-      public ProcessThread(Socket s){
+      //The current game this process is attached to
+      GameMaster.Game currentGame;
+      //The car representation this process is attached to
+      Car myCar;
+
+      public ProcessThread(Socket s, GameMaster.Game g){
          socket = s;
+         currentGame=g;
       }
    
       public void run() {
@@ -44,8 +53,6 @@ public class server {
             
             System.out.println("Accepting connection from ip " + ip);
 
-            
-            
             dis = new DataInputStream(socket.getInputStream());
             dos = new DataOutputStream(socket.getOutputStream());
 
@@ -54,96 +61,85 @@ public class server {
                String clientAction =  dis.readUTF();
                //do something about adding the two client together 
                if(clientAction.equals("READY&WAITING")){
-                  clientCars.add(readCar());
-                  //server waits for more clients 
-                  //then sends them to the client:
-                  //filler code
-                  clientCars.add(new car("Michele","red",0,5));
-                  clientCars.add(new car("Tim","blue",0,5));
-                  writeCar(clientCars.get(1));
-                  writeCar(clientCars.get(2));
-
-                  dos.writeUTF("START");
+                  Car car = readCar();
+                  myCar=car;
+                  currentGame.addRacer(car);
                }else if(clientAction.equals("STARTED")){
-                  updateClients(); 
-                  checkComplete();
-               }else if(clientAction.equals("STOPPED")){
-                  sendAllClients();//should also send amount of time or Words per min to calc it with 
+                  updateClient();
+                 // checkComplete() -- GameMaster will handle this
                }else if(clientAction.equals("CLOSE")){
-                  clientCars.remove(Integer.parseInt(dis.readUTF()));
+                  currentGame.removeCar(myCar.getID());
                   socket.close();
+               }else if(clientAction.equals("STARTREQUEST")){
+                  startRequest();
                }
             }
          }catch(Exception ex){
             ex.printStackTrace();
          }
       }
-      
-      public boolean checkComplete(){//sends either PLAYON or STOP to server 
-         for(car Car : clientCars){
-            if(Car.compleated()){
-               //then end game 
-               try{
-                  dos.writeUTF("STOP");
-               }catch(Exception ex){ }
-               return true;
-            }
-         }
+
+
+      public void startRequest(){
+         boolean result =currentGame.startGame(myCar.getID());
          try{
-            dos.writeUTF("PLAYON");
-         }catch(Exception ex){ }
-         return false;
+            dos.writeBoolean(result);
+         }catch (Exception ex){ }
+
       }
-      public void updateClients(){//updates word count of this client and sends others 
-         String index = null;
-         int _wordCount = 0;
+
+      public void removeCar(String uid){
          try{
-            index = dis.readUTF();
+            dos.writeUTF("REMOVECAR");
+            dos.writeUTF(uid);
+         }catch (Exception ex){ }
+
+      }
+
+      public void updateClient(){//updates word count of this client and sends others
+         int _wordCount = -1;
+         try{
             _wordCount = dis.readInt();
-            sendAllClients();
          }catch(Exception ex){ }
-         clientCars.get(Integer.parseInt(index)).setWordCount(_wordCount);
+         currentGame.updateRacer(myCar.getID(), _wordCount);
       }
-      public void sendAllClients(){//itterates through clientCars and sends their index and numWords
+
+      public void refreshClients(String uid, int wordCount){
          try{
-            for(int i = 0 ; i < clientCars.size(); i++){
-               dos.writeUTF(Integer.toString(i));
-               dos.writeInt(clientCars.get(i).getWordCount());
-            }
-         }catch(Exception ex){ }
+            dos.writeUTF("REFRESH");
+            dos.writeUTF(uid);
+            dos.writeInt(wordCount);
+         }catch (Exception ex){ }
       }
-      public void writeCar(car Car){//sends car object
+      public void writeCar(Car Car){//sends car object
          try{
             dos.writeUTF(Car.getName());
             dos.writeUTF(Car.getColor());
+            dos.writeUTF(Car.getID());
             dos.writeInt(Car.getWordCount());
-            dos.writeInt(Car.getMaxWords());
             dos.flush();
          }catch(Exception ex){}
       }
-      public car readCar(){//reds car object
+      public Car readCar(){//reds car object
          String name = null;
          String color = null;
          int wordCount = 0;
-         int maxWords = 0;
+
          try{
             name = dis.readUTF();
             color = dis.readUTF();
             wordCount = dis.readInt();
-            maxWords = dis.readInt();
+
          }catch(Exception ex){ }
-         return new car(name,color,wordCount,maxWords);
+         return new Car(name,color,wordCount);
       }
-      public boolean getReady(){//???
-          return ready;
-      }
-      public void setReady(boolean _ready){//???
-        ready = _ready;
-      }
-      public void runGame(){//????
-          try{
-              
-          }catch(Exception ex){ }
+
+      public void startGame(String sentence){
+      try{
+         dos.writeUTF("START");
+         dos.writeUTF(sentence);
+      }catch (Exception ex){ }
+
       }
    }
 }
